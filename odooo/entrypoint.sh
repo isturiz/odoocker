d007b4a11fb1e455c1ddcc067c35b275003fa8d0
Mauricio Istúriz <isturizmauricio@gmail.com>
2025-08-03 14:52:24 -0400

    requirements: read and install addon requirements
    Now, each requirement from custom addons is read

--- odooo/entrypoint.sh
+++ odooo/entrypoint.sh
@@ -21,6 +21,13 @@ echo "---- odoo.conf used ----"
 cat /home/odoo/odooo/odoo.conf
 echo "------------------------"
 
+# Install requirements.txt from all addons (recursive in /home/odoo/src)
+echo "Scanning addons for requirements.txt..."
+find /home/odoo/src -type f -name "requirements.txt" | while read -r reqfile; do
+    echo "Installing Python requirements from $reqfile"
+    pip3 install --no-cache-dir -r "$reqfile"
+done
+
 echo "Entrypoint running as $(whoami)"
 echo "Odoo version:"
 which odoo
